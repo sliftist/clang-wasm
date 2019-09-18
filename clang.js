664270ef74f814ce04798caf23a176a44f37f106
sliftist <sliftist@gmail.com>
2019-09-18 16:18:42 -0400

    init/config update (AUTO GENERATED COMMIT)

--- clang.js
+++ clang.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 let path = require("./index.js").getBinaryPath("clang");
-let args = process.argv.slice(1);
+let args = process.argv.slice(2);
 require("child_process").execFileSync(path, args, { stdio: "inherit" });
